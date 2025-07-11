43851d9293b1783c3bbce747cd19df3c64ecbeaf
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-07-11 19:52:07 -0300

    feat: fix UI errors;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
   )
   
   const subathonInit = "2025-07-11T18:00:00-03:00";
-  const subathonInitComponent = moment().diff(moment(subathonInit), "seconds") > 0 ? "A live está no ar!" : (Util.renderText(moment(subathonInit).format("DD/MM/YYYY [às] HH:mm [UTC]Z")));
+  const subathonInitComponent = moment().diff(moment(subathonInit), "seconds") > 0 ? "A live está no ar!" : (<>A live começa em {Util.renderText(moment(subathonInit).format("DD/MM/YYYY [às] HH:mm [UTC]Z"))}.</>);
   
   const btnDonation = "btn btn-default rounded-1 text-dark d-flex align-items-center gap-1";
   const styleBtnDonation = {backdropFilter: "blur(10px)", backgroundColor: "#FFFFFF60", border: "1px solid #FFFFFF50"};
@@ -23,7 +23,7 @@ function Home() {
     <Main className="Main">
       <div className={"d-flex flex-column gap-5"}>
         <section className={"text-center text-balance text-body"}>
-          <p>A live começa em {subathonInitComponent}.</p>
+          <p>{subathonInitComponent}</p>
           <p>Assista em <Link to={"https://twitch.tv/eskimozin"} className={"text-body"}>{Util.renderText("twitch.tv/eskimozin")}</Link></p>
         </section>
         
@@ -31,7 +31,7 @@ function Home() {
         
         <section className={"banner-campaign p-3 rounded-2 d-flex flex-column gap-3 align-items-center text-center text-balance text-white"}>
           <h3 className={"text-dark fs-3 m-0"}>Contribua com a Eskimothon, para o Eskimo ficar 1 ano em live!</h3>
-          <p>A live começa em {subathonInitComponent}.</p>
+          <p>{subathonInitComponent}</p>
           <div className={"d-flex flex-wrap justify-content-center align-items-center gap-2"}>
             <Link to={links["livepix"]} className={btnDonation} style={styleBtnDonation}>Live Pix {iconOpenInNewPage}</Link>
             <Link to={links["subsTwitch"]} className={btnDonation} style={styleBtnDonation}>Dar subs {iconOpenInNewPage}</Link>
