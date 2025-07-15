7895a3369e2798e4fe81caf3ecb798942db614fb
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-07-15 14:51:59 -0300

    feat: exibicao do tempo em dias; botoes de facil acesso para calcular tempo

--- src/components/LiveTimeCalculator/TimeDisplay.jsx
+++ src/components/LiveTimeCalculator/TimeDisplay.jsx
@@ -4,15 +4,17 @@ import PropTypes from "prop-types";
 const TimeDisplay = ({totalSeconds}) => {
   const formattedTime = useMemo(() => {
     if (isNaN(totalSeconds) || totalSeconds < 0) {
-      return {h: 0, m: 0, s: 0};
+      return {d: 0, h: 0, m: 0, s: 0};
     }
-    const h = Math.floor(totalSeconds / 3600).toLocaleString('pt-br');
+    const d = Math.floor(totalSeconds / 3600 / 24).toLocaleString('pt-br');
+    const h = Math.floor((totalSeconds / 3600) % 24).toLocaleString('pt-br');
     const m = Math.floor((totalSeconds % 3600) / 60).toLocaleString('pt-br');
     const s = Math.floor(totalSeconds % 60).toLocaleString('pt-br');
-    return {h, m, s};
+    return {d, h, m, s};
   }, [totalSeconds]);
   
-  const toInt = (value) => parseInt(value.replace(/\./, ""));
+  const toInt = (value) => parseInt(value.toString().replace(/\./, ""));
+  const dInt = toInt(formattedTime.d);
   const hInt = toInt(formattedTime.h);
   const mInt = toInt(formattedTime.m);
   const sInt = toInt(formattedTime.s);
@@ -22,10 +24,11 @@ const TimeDisplay = ({totalSeconds}) => {
       <span className={""}>Isso dá em tempo de live:</span>
       <h3 className={"mt-1 fs-3 fw-semibold text-success-emphasis text-balance"}>
         {`
-          ${hInt > 0 ? formattedTime.h + ` ${hInt > 1 ? "horas" : "hora"}` + `${(mInt > 0 || sInt > 0) ? "," : ""}` : ""}
+          ${dInt > 0 ? formattedTime.d + ` ${dInt > 1 ? "dias" : "dia"}` + `${(hInt > 0 || mInt > 0 || sInt > 0) ? ", " : ""}` : ""}
+          ${hInt > 0 ? formattedTime.h + ` ${hInt > 1 ? "horas" : "hora"}` + `${(mInt > 0 && sInt > 0) ? "," : (mInt > 0) ? " e" : ""}` : ""}
           ${mInt > 0 ? formattedTime.m + ` ${mInt > 1 ? "minutos" : "minuto"}` + `${(sInt > 0) ? " e" : ""}` : ""}
           ${sInt > 0 ? formattedTime.s + ` ${sInt > 1 ? "segundos" : "segundo"}` : ""}
-          ${(hInt <= 0 && mInt <= 0 && sInt <= 0) ? "nada" : ""}
+          ${(dInt <= 0 && hInt <= 0 && mInt <= 0 && sInt <= 0) ? "nada" : ""}
         `}
       </h3>
     </div>
--- src/components/LiveTimeCalculator/TimeToValueForm.jsx
+++ src/components/LiveTimeCalculator/TimeToValueForm.jsx
@@ -71,13 +71,15 @@ const TimeToValueForm = ({time, onTimeChange, clearTimes}) => {
       </label>
     </div>
     
-    <button type={"reset"} className={"btn btn-secondary rounded-1 d-flex align-items-center justify-content-center gap-1"} onClick={clearTimes}>
-      <span className={"text-sm"}>Limpar</span>
-      <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-        <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
-        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
-      </svg>
-    </button>
+    <div>
+      <button type={"reset"} className={"btn btn-secondary rounded-1 d-flex align-items-center justify-content-center gap-1"} onClick={clearTimes}>
+        <span className={"text-sm"}>Limpar</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+          <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+        </svg>
+      </button>
+    </div>
   </form>);
 };
 
