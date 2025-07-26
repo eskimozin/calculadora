ec4a48134ee835ad3611580645433ac3e5b35a88
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-07-26 00:00:42 -0300

    feat: tema comemorativo para modday

--- src/components/LiveTimeCalculator/LiveTimeCalculator.jsx
+++ src/components/LiveTimeCalculator/LiveTimeCalculator.jsx
@@ -1,6 +1,6 @@
 import './styles.css';
 
-import { useState, useMemo, useEffect } from 'react';
+import {useState, useMemo, useEffect} from 'react';
 
 import ModeSelector from './ModeSelector';
 import ValueToTimeForm from './ValueToTimeForm';
@@ -8,40 +8,43 @@ import TimeToValueForm from './TimeToValueForm';
 import TimeDisplay from './TimeDisplay';
 import ValueDisplay from './ValueDisplay';
 import AnimatedComponents from "../AnimatedComponent/AnimatedComponents.jsx";
+import {Link} from "react-router-dom";
+import links from "../../data/links.js";
+import moment from "moment";
 
 const LiveTimeCalculator = () => {
   const [calculationMode, setCalculationMode] = useState('toTime'); // 'toTime' or 'toValue'
-
-  const [values, setValues] = useState({ real: '10', subs: '', bits: '' });
-  const [time, setTime] = useState({ hours: '1', minutes: '', seconds: '' });
-
+  
+  const [values, setValues] = useState({real: '10', subs: '', bits: ''});
+  const [time, setTime] = useState({hours: '1', minutes: '', seconds: ''});
+  
   const focusElement = (id) => {
     setTimeout(() => {
       document.getElementById(id).focus();
     }, 100);
   }
-
+  
   useEffect(() => {
     if (calculationMode === 'toTime') focusElement("real");
     else if (calculationMode === 'toValue') focusElement("hours");
   }, [calculationMode])
-
+  
   const handleValueChange = (name, value) => {
-    setValues(prev => ({ ...prev, [name]: value }));
+    setValues(prev => ({...prev, [name]: value}));
   };
-
+  
   const clearValues = () => {
-    setValues({ real: '0', subs: '', bits: '' });
+    setValues({real: '0', subs: '', bits: ''});
   }
-
+  
   const handleTimeChange = (name, value) => {
-    setTime(prev => ({ ...prev, [name]: value }));
+    setTime(prev => ({...prev, [name]: value}));
   };
-
+  
   const clearTimes = () => {
-    setTime({ hours: '0', minutes: '', seconds: '' });
+    setTime({hours: '0', minutes: '', seconds: ''});
   }
-
+  
   const totalSeconds = useMemo(() => {
     if (calculationMode === 'toTime') {
       const realMinutes = parseFloat(values.real) || 0;
@@ -55,33 +58,52 @@ const LiveTimeCalculator = () => {
       return h * 3600 + m * 60 + s;
     }
   }, [calculationMode, values, time]);
-
+  
+  const moddayInit = "2025-07-26T00:00:00-03:00";
+  const moddayFinish = "2025-07-27T00:00:00-03:00";
+  
+  const isModday = moment().diff(moment(moddayInit), "m") >= 0 && moment().diff(moment(moddayFinish), "m") < 0;
+  
   return (
     <AnimatedComponents>
-      <div className="calculator-container text-white card bg-dark d-flex flex-column gap-5">
-        <div className={"d-flex flex-column gap-3"}>
-          <p className={"fw-semibold mb-3 text-center text-balance fs-3"} style={{ lineHeight: "1.25" }}>
-            <span className={"text-balance"}>{calculationMode === "toTime" && "Descubra o tempo que ele ficará em live a partir de uma forma de contribuição"}</span>
-            <span className={"text-balance"}>{calculationMode === "toValue" && "Descubra quanto é necessário para ele ficar X tempo ao vivo - em dinheiros, subs e bits"}</span>
-          </p>
-          <ModeSelector mode={calculationMode} onModeChange={setCalculationMode} />
+      <div className="">
+        <div className={"bg-warning py-1 px-3 text-black"} style={{borderRadius: "7.5px 7.5px 0 0", border: ""}}></div>
+        <div className={"calculator-container px-3 py-3 d-flex flex-column text-white card bg-dark d-flex flex-column gap-5 p-0"} style={{borderRadius: "0 0 5px 5px"}}>
+          {
+            isModday ? (
+              <div className="scrolling-container">
+                <div className="scrolling-text">
+                  <p className={"m-0 text-white"}>Hoje é modday! Por favor doe para que os mods não tenham que morar na rua. Cada 1 real doado volta para você, pois eles garantem uma comunidade saudável, eventos maravilhosos e sempre gente boa no chat!</p>
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )
+          }
+          <div className={"d-flex flex-column gap-3"}>
+            <p className={"fw-semibold mb-3 text-center text-balance fs-3"} style={{lineHeight: "1.25"}}>
+              <span className={"text-balance"}>{calculationMode === "toTime" && "Descubra o tempo que ele ficará em live a partir de uma forma de contribuição"}</span>
+              <span className={"text-balance"}>{calculationMode === "toValue" && "Descubra quanto é necessário para ele ficar X tempo ao vivo - em dinheiros, subs e bits"}</span>
+            </p>
+            <ModeSelector mode={calculationMode} onModeChange={setCalculationMode}/>
+          </div>
+          
+          {calculationMode === 'toTime' ? (
+            <AnimatedComponents>
+              <div className="d-flex flex-column gap-3 mt-2">
+                <ValueToTimeForm values={values} onValueChange={handleValueChange} clearValues={clearValues}/>
+                <TimeDisplay totalSeconds={totalSeconds} values={values}/>
+              </div>
+            </AnimatedComponents>
+          ) : (
+            <AnimatedComponents>
+              <div className="d-flex flex-column gap-3 mt-2">
+                <TimeToValueForm time={time} onTimeChange={handleTimeChange} clearTimes={clearTimes}/>
+                <ValueDisplay totalSeconds={totalSeconds}/>
+              </div>
+            </AnimatedComponents>
+          )}
         </div>
-
-        {calculationMode === 'toTime' ? (
-          <AnimatedComponents>
-            <div className="d-flex flex-column gap-3">
-              <ValueToTimeForm values={values} onValueChange={handleValueChange} clearValues={clearValues} />
-              <TimeDisplay totalSeconds={totalSeconds} values={values} />
-            </div>
-          </AnimatedComponents>
-        ) : (
-          <AnimatedComponents>
-            <div className="d-flex flex-column gap-3">
-              <TimeToValueForm time={time} onTimeChange={handleTimeChange} clearTimes={clearTimes} />
-              <ValueDisplay totalSeconds={totalSeconds} />
-            </div>
-          </AnimatedComponents>
-        )}
       </div>
     </AnimatedComponents>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import { Main } from "../components/Main";
+import {Main} from "../components/Main";
 import LiveTimeCalculator from "../components/LiveTimeCalculator/LiveTimeCalculator.jsx";
 import moment from "moment";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Util from "../assets/Util.jsx";
 import links from "../data/links.js";
 import resources from "../data/resources.js";
@@ -10,16 +10,22 @@ import AnimatedComponents from "../components/AnimatedComponent/AnimatedComponen
 function Home() {
   const iconOpenInNewPage = (
     <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="" className={"text-dark"}>
-      <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
+      <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/>
     </svg>
   )
-
+  
   const subathonInit = "2025-07-11T18:00:00-03:00";
-  const subathonInitComponent = moment().diff(moment(subathonInit), "seconds") > 0 ? <span style={{ color: "inherit" }}>A live está no ar!</span> : (<span className="text-black">A live começa em {Util.renderText(moment(subathonInit).format("DD/MM/YYYY [às] HH:mm [UTC]Z"))}.</span>);
-
+  const subathonInitComponent = moment().diff(moment(subathonInit), "seconds") > 0 ? <span style={{color: "inherit"}}>A live está no ar!</span> : (<span className="text-black">A live começa em {Util.renderText(moment(subathonInit).format("DD/MM/YYYY [às] HH:mm [UTC]Z"))}.</span>);
+  
+  const moddayInit = "2025-07-26T00:00:00-03:00";
+  const moddayFinish = "2025-07-27T00:00:00-03:00";
+  const moddayAnnouncementComponent = <div className={"d-block mb-3 fw-medium"}>Hoje é <span className={"text-warning"}>modday!</span> Isso significa que <span className={"text-warning"}>todos os donates feitos via <Link to={links["livepix"]} className={"text-decoration-none text-warning fw-bolder"}>LIVE PIX</Link> vão para os magnânimos moderadores</span> que tantam ajudam a live e a comunidade :D</div>
+  
+  const isModday = moment().diff(moment(moddayInit), "m") >= 0 && moment().diff(moment(moddayFinish), "m") < 0;
+  
   const btnDonation = "btn btn-default rounded-1 text-dark d-flex align-items-center gap-1";
-  const styleBtnDonation = { backdropFilter: "blur(10px)", backgroundColor: "#FFFFFF60", border: "1px solid #FFFFFF50" };
-
+  const styleBtnDonation = {backdropFilter: "blur(10px)", backgroundColor: "#FFFFFF60", border: "1px solid #FFFFFF50"};
+  
   return (
     <Main className="Main">
       {
@@ -35,19 +41,25 @@ function Home() {
           <AnimatedComponents>
             <div className={"d-flex flex-column gap-5"}>
               <section className={"text-center text-balance text-body"}>
-                <p>{subathonInitComponent}</p>
-                <p>Assista em <Link to={"https://twitch.tv/eskimozin"} className={"text-body"}>{Util.renderText("twitch.tv/eskimozin")}</Link></p>
+                <p>{isModday ? moddayAnnouncementComponent : subathonInitComponent}</p>
+                {isModday ? <p>Doe agora mesmo em <Link to={links["livepix"]} className={"text-body text-decoration-none fw-bolder text-warning-emphasis"}>{Util.renderText(links["livepix"].replace("https://", ""))}</Link></p> : <p>Assista em <Link to={"https://twitch.tv/eskimozin"} className={"text-body"}>{Util.renderText("twitch.tv/eskimozin")}</Link></p>}
               </section>
-
-              <LiveTimeCalculator />
-
+              
+              <LiveTimeCalculator/>
+              
               <section className={"banner-campaign p-3 rounded-2 d-flex flex-column gap-3 align-items-center text-center text-balance"}>
-                <h3 className={"text-dark fs-3 m-0"}>Contribua com a Eskimothon, para o Eskimo ficar 1 ano em live!</h3>
-                <p className="text-black">{subathonInitComponent}</p>
+                <h3 className={"text-dark fs-3 m-0"}>{isModday ? "Contribua com o modday, para que os mods não precisem morar na rua!" : "Contribua com a Eskimothon, para o Eskimo ficar 1 ano em live!"}</h3>
+                <p className="text-black">{isModday ? "Doe agora mesmo pelo Live Pix!" : subathonInitComponent}</p>
                 <div className={"d-flex flex-wrap justify-content-center align-items-center gap-2"}>
                   <Link to={links["livepix"]} className={btnDonation} style={styleBtnDonation}>Live Pix {iconOpenInNewPage}</Link>
-                  <Link to={links["subsTwitch"]} className={btnDonation} style={styleBtnDonation}>Dar subs {iconOpenInNewPage}</Link>
-                  <Link to={links["bitsTwitch"]} className={btnDonation} style={styleBtnDonation}>Contribuir com bits {iconOpenInNewPage}</Link>
+                  {
+                    !isModday && (
+                      <>
+                        <Link to={links["subsTwitch"]} className={btnDonation} style={styleBtnDonation}>Dar subs {iconOpenInNewPage}</Link>
+                        <Link to={links["bitsTwitch"]} className={btnDonation} style={styleBtnDonation}>Contribuir com bits {iconOpenInNewPage}</Link>
+                      </>
+                    )
+                  }
                 </div>
                 <div>
                   <p className={"m-0 text-black"}>{resources.valDonation} real dá {resources.donationInTimeMinutes} minuto de live, <b>{resources.valSubs} sub dá {resources.subsInTimeMinutes} minutos</b> e {resources.valBits} bits vira {resources.bitsInTimeMinutes} minutos!</p>
